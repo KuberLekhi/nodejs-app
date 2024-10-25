8c172825989c800e1df62c85dfad5174794412d5
Kuber Lekhi <155307227+KuberLekhi@users.noreply.github.com>
2024-10-26 00:54:59 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./mydatabase.db');
 
 // Handle form submission
 router.post('/submit', (req, res) => {
     const { name, age, color } = req.body;
     console.log(`Name: ${name}, Age: ${age}, Favorite Color: ${color}`);
-    res.send(`<h1>Thank you, ${name}!</h1><p>Your details have been saved.</p>`);
+    
+    // Save the data to the SQLite database
+    db.run(`INSERT INTO data (temperature, humidity) VALUES (?, ?)`, [age, color], function(err) {
+        if (err) {
+            return res.status(500).send("Error storing data");
+        }
+        res.send(`<h1>Thank you, ${name}!</h1><p>Your details have been saved.</p>`);
+    });
+});
+
+// Route to get all data
+router.get('/data', (req, res) => {
+    db.all(`SELECT * FROM data`, [], (err, rows) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error retrieving data");
+        }
+        res.json(rows);
+    });
 });
 
 module.exports = router;
